5789ea901d7c1e5cdb106c32c27322df0a8b6018
Chayan8837 <145531182+Chayan8837@users.noreply.github.com>
2024-08-24 01:26:47 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -32,7 +32,8 @@ app.use(cors({
     origin: [
       'http://localhost:5174',
       'http://localhost:5173',
-      'https://shopperweb.vercel.app'
+      'https://shopperweb.vercel.app',
+      'https://shopperadmin.vercel.app'
       
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
